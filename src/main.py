238e86ff131a9657b00e73497555cec349ae2f76
FrinshHD <62144694+FrinshHD@users.noreply.github.com>
2025-06-01 20:02:48 +0000

    refactor: remove unused dashboard, logs viewer, player info, server management, and server status modules; update requirements

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
-from dashboard import Dashboard
+import typer
 
-class MinecraftServerMonitorApp(App):
-    def compose(self) -> ComposeResult:
-        yield Header()
-        yield Dashboard()  # <-- parentheses to instantiate
-        yield Footer()
+app = typer.Typer()
+
+
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
 
 if __name__ == "__main__":
-    MinecraftServerMonitorApp().run()
+    app()
